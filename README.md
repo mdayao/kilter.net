f88f7aad0a905a7f9c35cc0c54fe6c5465de1d6b
Abhinav Adduri <aadduri@andrew.cmu.edu>
2023-04-08 01:15:17 -0400

    added script to generate the training data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kilter.net
+
+To generate the training data, go into the data_preprocessing folder and run: `python clean_kilter_df.py`
---
+++ data_preprocessing/clean_kilter_df.py
@@ -0,0 +1,95 @@
+import json
+import numpy as np
+
+all_data = json.load(open('all_holds.json'))
+
+main_hold_to_grid = {}
+main_hold_to_coords = {}
+
+# 18 by 17
+for idx, hold_idx in enumerate(all_data[0][0]['mainholds']['hold_id']):
+    main_hold_to_grid[hold_idx] = all_data[0][0]['mainholds']['grid'][idx]
+    main_hold_to_coords[hold_idx] = all_data[0][0]['mainholds']['coords'][idx]
+
+# 15 by 18
+aux_hold_to_grid = {}
+aux_hold_to_coords = {}
+for idx, hold_idx in enumerate(all_data[0][1]['auxillary']['hold_id']):
+    aux_hold_to_grid[hold_idx] = all_data[0][1]['auxillary']['grid'][idx]
+    aux_hold_to_coords[hold_idx] = all_data[0][1]['auxillary']['coords'][idx]
+
+# 2 by 18
+kick_hold_to_grid = {}
+kick_hold_to_coords = {}
+for idx, hold_idx in enumerate(all_data[0][2]['kickboard']['hold_id']):
+    kick_hold_to_grid[hold_idx] = all_data[0][2]['kickboard']['grid'][idx]
+    kick_hold_to_coords[hold_idx] = all_data[0][2]['kickboard']['coords'][idx]
+
+# convert a list of holds and hold types into a one hot encoding
+def convert_hold_to_one_hot(hold_idxs, channels):
+    final_grid = np.zeros((4, 35, 18))
+    for channel, hold_idx in zip(channels, hold_idxs):
+        # classify if this hold is a main hold, auxillary hold, or kick hold
+        if hold_idx in main_hold_to_grid:
+            main_j, main_i = main_hold_to_grid[hold_idx]
+            final_grid[channel, main_i, main_j] = 1
+
+        elif hold_idx in aux_hold_to_grid:
+            aux_j, aux_i = aux_hold_to_grid[hold_idx]
+            final_grid[channel, aux_i, aux_j] = 1
+
+        elif hold_idx in kick_hold_to_grid:
+            kick_j, kick_i = kick_hold_to_grid[hold_idx]
+            final_grid[channel, kick_i, kick_j] = 1
+
+    assert(np.sum(final_grid) > 1)
+    return final_grid
+
+def htype_to_channel(htype):
+    if htype == '12':
+        return 0
+    elif htype == '13':
+        return 1
+    elif htype == '14':
+        return 2
+    else:
+        return 3
+
+csv = open('kilter_df.csv')
+next(csv)
+
+climbs = []
+vgrades = []
+ascents = []
+qualities = []
+angles = []
+
+for line in csv:
+    toks = line.strip().split(',')[1:]
+    # ['uuid', 'board_angle', 'frames', 'quality_average', 'v_grade', 'ascensionist_count']
+    uuid, board_angle, frames, quality_average, vgrade, ascensionist_count = toks
+    frames = frames.split('p')[1:]
+
+    # holds and hold types (converted into channel)
+    holds = [int(hold_and_type.split('r')[0]) for hold_and_type in frames]
+    hold_types = [htype_to_channel(hold_and_type.split('r')[1]) for hold_and_type in frames]
+
+    curr_climb_one_hot = convert_hold_to_one_hot(holds, hold_types)
+
+    climbs.append(curr_climb_one_hot)
+    vgrades.append(float(vgrade))
+    ascents.append(float(ascensionist_count))
+    qualities.append(float(quality_average))
+    angles.append(float(board_angle))
+
+climbs = np.array(climbs)
+vgrades = np.array(vgrades)
+ascents = np.array(ascents)
+qualities = np.array(qualities)
+angles = np.array(angles)
+
+np.save('../training_data/kilter_climb_features.npy', climbs)
+np.save('../training_data/kilter_vgrades.npy', vgrades)
+np.save('../training_data/kilter_ascents.npy', ascents)
+np.save('../training_data/kilter_qualities.npy', qualities)
+np.save('../training_data/kilter_angles.npy', angles)
