d8ca539891f79447c65037873400f9be99d898eb
Drew McNutt <anm329@pitt.edu>
2023-04-10 23:34:50 -0400

    figure dir

--- generate.py
+++ generate.py
@@ -19,13 +19,15 @@ from lightning.pytorch.loggers import WandbLogger
 
 parser = argparse.ArgumentParser(description='Running kilter.net CVAE')
 
-parser.add_argument('--path_to_checkpoint', help='path to saved model checkpoint')
+parser.add_argument('--path-to-checkpoint','-P', required=True, help='path to saved model checkpoint')
 parser.add_argument('--angle','-A', default=40, type=int, help='angle of the wall')
 parser.add_argument('--grade','-V', default=1,type=int, help='V-grade of the problem')
 parser.add_argument('--latent-dim','-L', type=int, default=32, help='Latent dimension')
 parser.add_argument('--learning-rate', '-lr', type=float, default=1e-3, help='Learning rate')
 parser.add_argument('--batch-size', '-bs', type=int, default=128, help='Batch size')
 parser.add_argument('--kld-weight', '-kld', type=float, default=2.5e-4, help='KL divergence weight')
+
+parser.add_argument('--figure_dir','-F', default="figures", help='Directory to save figures')
 
 args = parser.parse_args()
 
@@ -48,6 +50,6 @@ print(predicted_out.max())
 print(predicted_out.min())
 for i in range(10):
     fig = plot_climb(predicted_out[i])
-    plt.savefig(f'./figures/generated_climbs/{i}.png',dpi=300)
+    plt.savefig(f'./{args.figure_dir}/generated_climbs/{i}.png',dpi=300)
 
 
