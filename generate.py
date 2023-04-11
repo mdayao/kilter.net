d34cece48fb2dc7c71c57b92d812ee18d635e619
Drew McNutt <anm329@pitt.edu>
2023-04-11 08:43:07 -0400

    less printing and threshold value argument

--- generate.py
+++ generate.py
@@ -27,36 +27,25 @@ parser.add_argument('--learning-rate', '-lr', type=float, default=1e-3, help='Le
 parser.add_argument('--batch-size', '-bs', type=int, default=128, help='Batch size')
 parser.add_argument('--kld-weight', '-kld', type=float, default=2.5e-4, help='KL divergence weight')
 
+parser.add_argument('--threshold', '-t', type=float, default=0.95, help='Threshold for binarization')
+
 parser.add_argument('--path-to-checkpoint','-P', required=True, help='path to saved model checkpoint')
 parser.add_argument('--figure_dir','-F', default="figures/generated_climbs/", help='Directory to save figures')
 
 args = parser.parse_args()
 
-#base_model = CVAE(in_channels=4,
-#             latent_dim=args.latent_dim, 
-#             )
-# experiment = VAEXperiment(base_model,
-#                           params = {
-#                               'kld_weight': args.kld_weight,
-#                               'LR': args.learning_rate
-#                               })
 trained_model = VAEXperiment.load_from_checkpoint(args.path_to_checkpoint, vae_model=CVAE(in_channels=4, latent_dim=args.latent_dim))
 trained_model.eval()
-#checkpoint = torch.load(args.path_to_checkpoint,map_location=torch.device('cpu'))
-#print(checkpoint['state_dict'].keys())
-#base_model.load_state_dict(checkpoint['state_dict'])
+
 labels = torch.tensor([args.grade, args.angle],dtype=float)
 predicted_out = trained_model.model.sample(labels, num_samples=args.num_samples)
 predicted_climbs = predicted_out.detach().numpy()
-# print(predicted_out.max())
-# print(predicted_out.min())
-# print(predicted_climbs[0].shape)
+
 for i in range(args.num_samples):
     curr_climb = predicted_climbs[i]
+    print(f"Climb {idx}:\n\tMin: {curr_climb.min()}, Max: {curr_climb.max()}")
     curr_climb = (curr_climb - curr_climb.min())/(curr_climb.max() - curr_climb.min())
-    print(curr_climb.shape)
-    curr_climb = np.where(curr_climb > 0.95,1,0)
-    print(curr_climb.shape)
+    curr_climb = np.where(curr_climb > args.threshold,1,0)
     fig = plot_climb(curr_climb)
     plt.savefig(f'./{args.figure_dir}/grade{args.grade}_angle{args.angle}_{i}.png',dpi=300)
 
